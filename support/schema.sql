5f8f31709a642838f80c0760c7356c4f3bfcf3e0
dschreiber4 <dschreiber4@student.cvtc.edu>
2021-11-05 18:05:23 -0500

    db tables

--- support/schema.sql
+++ support/schema.sql
@@ -21,7 +21,8 @@ CREATE TABLE observations (
 	id SERIAL PRIMARY KEY,
 	users_id INT NOT NULL,
 	students_id INT NOT NULL,
-	tasks_id INT NOT NULL
+	tasks_id INT NOT NULL,
+	duration INTERVAL NOT NULL
 );
 
 CREATE TABLE tasks (
